8d55fe330c1c446778178e3c366c959c9d45052c
Bryon Burke and Dominique Youmans <dominique@>
2019-10-23 10:44:29 -0700

    added upper case conversion

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var upperCase = person1Input.toUpperCase();
+    console.log(upperCase)
+    $(".person1").text(upperCase);
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+
 
     $("#story").show();
     $("#hide").hide();
